46f3b06ec715828ff608b1ce60e88d1de0c5cdc3
mmtg445 <mmtg445@gmail.com>
2024-11-11 19:45:59 +0600

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('21188057', ''))
-API_HASH = environ.get('8564fab8db759bb04b1907bd12ed98ef', '')
+API_ID = int(environ.get('API_ID', '21188057'))
+API_HASH = environ.get('API_HASH', '8564fab8db759bb04b1907bd12ed98ef')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 PORT = environ.get("PORT", "8080")
 
