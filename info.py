45d0d9d823ecf5abe027ee8b61b5836c36430a37
mmtg445 <mmtg445@gmail.com>
2024-11-23 22:14:29 +0600

    Update info.py

--- info.py
+++ info.py
@@ -75,6 +75,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
+P_TTI_SHOW_OFF = bool(environ.get("P_TTI_SHOW_OFF", False))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
