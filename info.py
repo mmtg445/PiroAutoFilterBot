67d119a9816f0cfebbe6d67541934784a97fe544
mmtg445 <mmtg445@gmail.com>
2024-11-23 22:28:28 +0600

    Update info.py

--- info.py
+++ info.py
@@ -44,10 +44,10 @@ auth_channel = environ.get('AUTH_CHANNEL', '-1002196942185')
 auth_grp = environ.get('-1002382543304 -1002164413929')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002329322641')
+support_chat = environ.get('SUPPORT_CHAT', '-1002329322641')
 reqst_channel = environ.get('REQST_CHANNEL', '-1002479416190')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-SUPPORT_CHAT_ID = "SUPPORT_CHAT_ID", "-4574811926"
+SUPPORT_CHAT_ID = "SUPPORT_CHAT_ID", "-1002470486707"
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
